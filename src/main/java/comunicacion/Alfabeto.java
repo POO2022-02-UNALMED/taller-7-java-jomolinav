0dfe1306fe336d7527e6da64655eaeebe0b1ad92
jomolinav <111089102+jomolinav@users.noreply.github.com>
2022-10-10 23:48:43 -0500

    27

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -32,8 +32,8 @@ private String interpretacion;
 	public String interpretacion() {
 		return interpretacion;
 }
-	public String toString() {
-		return "A" + "B" + "C" + "D" + "E" + "F" + "G" +" H" + "I" + "J" + "K" + "L" + "M" + "N" + "O" +"P" +"Q" + "R" +"S" + "T" + "U" + "V" + "W" + "Y" + "Z";
+		public String toString() {
+		return ("A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z");	
 		
 }
 }
